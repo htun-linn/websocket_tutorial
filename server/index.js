1689e0121e0fee50f675fece86e47889eb6b3395
htunlin <work.htunlin@gmail.com>
2025-08-12 16:54:12 +0630

    lesson 3: use express with socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
-import { createServer } from "http"
+import express from 'express'
 import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const httpServer = createServer()
+const PORT = process.env.PORT || 3500
+const app = express()
 
-const io = new Server(httpServer, {
+app.use(express.static(path.join(__dirname, "public")))
+
+const expresServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expresServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
@@ -19,5 +30,3 @@ io.on('connection', socket => {
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 })
-
-httpServer.listen(3500, () => console.log('listening on port 3500'))
